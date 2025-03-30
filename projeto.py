c43fb2b43813adbebe9594fa0ebd2f84e00f9bcc
Vibelo <vibelo_git@outlook.com>
2025-03-30 10:47:28 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -5,15 +5,24 @@ class GerarExceção(Exception):
 class Cubo:
 
     def __init__(self):
-        self.face0 = [[None,None,None],[None,None,None],[None,None,None]] #Portugol não possui listas dinâmicas, então preciso fazer assim para me aproximar ao máximo
-        self.face1 = [[None,None,None],[None,None,None],[None,None,None]] #do portugol, com listas de tamanhos já pré definidos
-        self.face2 = [[None,None,None],[None,None,None],[None,None,None]]
-        self.face3 = [[None,None,None],[None,None,None],[None,None,None]]
-        self.face4 = [[None,None,None],[None,None,None],[None,None,None]]
-        self.face5 = [[None,None,None],[None,None,None],[None,None,None]]
+        #self.face0 = [[None,None,None],[None,None,None],[None,None,None]] #Portugol não possui listas dinâmicas, então preciso fazer assim para me aproximar ao máximo
+        #self.face1 = [[None,None,None],[None,None,None],[None,None,None]] #do portugol, com listas de tamanhos já pré definidos
+        #self.face2 = [[None,None,None],[None,None,None],[None,None,None]]
+        #self.face3 = [[None,None,None],[None,None,None],[None,None,None]]
+        #self.face4 = [[None,None,None],[None,None,None],[None,None,None]]
+        #self.face5 = [[None,None,None],[None,None,None],[None,None,None]]
+
+        self.cubo = {
+            0: [[None,None,None],[None,None,None],[None,None,None]],
+            1: [[None,None,None],[None,None,None],[None,None,None]],
+            2: [[None,None,None],[None,None,None],[None,None,None]],
+            3: [[None,None,None],[None,None,None],[None,None,None]],
+            4: [[None,None,None],[None,None,None],[None,None,None]],
+            5: [[None,None,None],[None,None,None],[None,None,None]]
+        }
 
     # Esta função corrige a face girando-a no sentido anti horário
-    def __corrigir_face_adjacente(self,face: list[list]) -> list[list]: #utilizamos o _ para tornar o método privado utilizável somente pela classe
+    def __corrigir_face_adjacente(self,face: list[list[str]]) -> list[list]: #utilizamos o _ para tornar o método privado utilizável somente pela classe
 
         linha_1 = face[0].copy() #criamos uma cópia da face, o que nos ajuda na hora de realizar a rotação
         linha_2 = face[1].copy() #pois a forma original não é perdida, logo ainda temos as informações
@@ -34,27 +43,26 @@ class Cubo:
         return face
         
 
-
     def rotacionar_no_sentido_anti_horário_na_horizontal(self, camada : int) -> None: #Horizontal(⬅️➡️)
 
-        camada_face0 = self.face3[camada].copy() #aqui nós estamos salvando a camada dada como parâmetro
-        camada_face1 = self.face0[camada].copy() #caso tenha sido escolhida a camada 1, por exemplo
-        camada_face2 = self.face1[camada].copy() #então nós iremos salvar a camada 1 de todas as faces
-        camada_face3 = self.face2[camada].copy()
+        camada_face0 = self.cubo[3][camada].copy() #aqui nós estamos salvando a camada dada como parâmetro
+        camada_face1 = self.cubo[0][camada].copy() #caso tenha sido escolhida a camada 1, por exemplo
+        camada_face2 = self.cubo[1][camada].copy() #então nós iremos salvar a camada 1 de todas as faces
+        camada_face3 = self.cubo[2][camada].copy()
 
-        self.face0[camada] = camada_face0 # e logo em seguida realizamos as trocas, permitindo assim o nosso movimento
-        self.face1[camada] = camada_face1
-        self.face2[camada] = camada_face2
-        self.face3[camada] = camada_face3
+        self.cubo[0][camada] = camada_face0 # e logo em seguida realizamos as trocas, permitindo assim o nosso movimento
+        self.cubo[1][camada] = camada_face1
+        self.cubo[2][camada] = camada_face2
+        self.cubo[3][camada] = camada_face3
 
         if camada == 0: #a face 4 é afetada
 
-            self.face4 = self.__corrigir_face_adjacente(self.face4)
+            self.cubo[4] = self.__corrigir_face_adjacente(self.cubo[4])
 
 
         elif camada == 2: #a face 5 é afetada
 
-            self.face5 = self.__corrigir_face_adjacente(self.face5)
+            self.cubo[5] = self.__corrigir_face_adjacente(self.cubo[5])
 
 
     def rotacionar_no_sentido_horário_na_horizontal(self, camada):
@@ -70,28 +78,28 @@ class Cubo:
     def rotacionar_no_sentido_anti_horário_na_vertical(self, coluna : int):
         if coluna in [0,1,2]:
 
-            coluna0 = [self.face0[0][coluna], self.face0[1][coluna], self.face0[2][coluna]] #aqui não estamos nos referindo a primeira coluna da primeira face, segunda coluna da primeira face etc. coluna1 é a primeira coluna
-            coluna1 = [self.face4[0][coluna], self.face4[1][coluna], self.face4[2][coluna]] #aqui não estamos nos referindo a primeira coluna da primeira face, segunda coluna da primeira face etc. coluna1 é a primeira coluna
-            coluna2 = [self.face2[0][coluna], self.face2[1][coluna], self.face2[2][coluna]] #da face 1, coluna2 é a primeira coluna da face superior (5), ou seja, é a sequência que iremos seguir. No caso de ser a primeira
-            coluna3 = [self.face5[0][coluna], self.face5[1][coluna], self.face5[2][coluna]] #é somente se a coluna que a pessoa escolher for a coluna 0.
+            coluna0 = [self.cubo[0][0][coluna], self.cubo[0][1][coluna], self.cubo[0][2][coluna]] #aqui não estamos nos referindo a primeira coluna da primeira face, segunda coluna da primeira face etc. coluna1 é a primeira coluna
+            coluna1 = [self.cubo[4][0][coluna], self.cubo[4][1][coluna], self.cubo[4][2][coluna]] #aqui não estamos nos referindo a primeira coluna da primeira face, segunda coluna da primeira face etc. coluna1 é a primeira coluna
+            coluna2 = [self.cubo[2][0][coluna], self.cubo[2][1][coluna], self.cubo[2][2][coluna]] #da face 1, coluna2 é a primeira coluna da face superior (5), ou seja, é a sequência que iremos seguir. No caso de ser a primeira
+            coluna3 = [self.cubo[5][0][coluna], self.cubo[5][1][coluna], self.cubo[5][2][coluna]] #é somente se a coluna que a pessoa escolher for a coluna 0.
 
 
 
-            self.face0[coluna][0] = coluna3[2]
-            self.face0[coluna][1] = coluna3[1]
-            self.face0[coluna][2] = coluna3[0]
+            self.cubo[0][coluna][0] = coluna3[2]
+            self.cubo[0][coluna][1] = coluna3[1]
+            self.cubo[0][coluna][2] = coluna3[0]
 
-            self.face4[coluna][0] = coluna0[2]
-            self.face4[coluna][1] = coluna0[1]
-            self.face4[coluna][2] = coluna0[0]
+            self.cubo[4][coluna][0] = coluna0[2]
+            self.cubo[4][coluna][1] = coluna0[1]
+            self.cubo[4][coluna][2] = coluna0[0]
             
-            self.face2[coluna][0] = coluna1[2]
-            self.face2[coluna][1] = coluna1[1]
-            self.face2[coluna][2] = coluna1[0]
+            self.cubo[2][coluna][0] = coluna1[2]
+            self.cubo[2][coluna][1] = coluna1[1]
+            self.cubo[2][coluna][2] = coluna1[0]
 
-            self.face5[coluna][0] = coluna2[2]
-            self.face5[coluna][1] = coluna2[1]
-            self.face5[coluna][2] = coluna2[0]
+            self.cubo[5][coluna][0] = coluna2[2]
+            self.cubo[5][coluna][1] = coluna2[1]
+            self.cubo[5][coluna][2] = coluna2[0]
 
 
             
@@ -101,36 +109,39 @@ class Cubo:
             coluna = coluna - 3 #pois esta variável além de indicar a coluna, também serve para acessarmos os elementos na lista. Subtraimos 3, pois
             #os índices só vão até 2, ou seja, temos 3 índices por matriz: 0,1,2
 
-            coluna0 = [self.face1[0][coluna], self.face1[1][coluna], self.face1[2][coluna]] #aqui não estamos nos referindo a primeira coluna da primeira face, segunda coluna da primeira face etc. coluna1 é a primeira coluna
-            coluna1 = [self.face4[coluna][0], self.face4[coluna][1], self.face4[coluna][2]] #aqui não estamos nos referindo a primeira coluna da primeira face, segunda coluna da primeira face etc. coluna1 é a primeira coluna
-            coluna2 = [self.face3[0][coluna], self.face3[1][coluna], self.face3[2][coluna]] #da face 1, coluna2 é a primeira coluna da face superior (5), ou seja, é a sequência que iremos seguir. No caso de ser a primeira
-            coluna3 = [self.face5[coluna][0], self.face5[coluna][1], self.face5[coluna][2]] #é somente se a coluna que a pessoa escolher for a coluna 0.
+            tabela_de_tradução = {0:2, 1:1,2:0} #na face 1 nós podemos acessar a coluna 2 somente acessando o índice 2 de cada linha, mas na face 4
+            #é um pouco diferente. para que continuemos a linha reta, caso acessemos a coluna 0, então teremos que acessar a linha 2 da face 4.
+            #aqui está sendo utilizado um dicionário para facilitar a conversão.
+
+            coluna0 = [self.cubo[1][0][coluna], self.cubo[1][1][coluna], self.cubo[1][2][coluna]] #aqui não estamos nos referindo a primeira coluna da primeira face, segunda coluna da primeira face etc. coluna1 é a primeira coluna
+            coluna1 = [self.cubo[4][tabela_de_tradução[coluna]][0], self.cubo[4][tabela_de_tradução[coluna]][1], self.cubo[4][tabela_de_tradução[coluna]][2]] #aqui não estamos nos referindo a primeira coluna da primeira face, segunda coluna da primeira face etc. coluna1 é a primeira coluna
+            coluna2 = [self.cubo[3][0][coluna], self.cubo[3][1][coluna], self.cubo[3][2][coluna]] #da face 1, coluna2 é a primeira coluna da face superior (5), ou seja, é a sequência que iremos seguir. No caso de ser a primeira
+            coluna3 = [self.cubo[5][tabela_de_tradução[coluna]][0], self.cubo[5][tabela_de_tradução[coluna]][1], self.cubo[5][tabela_de_tradução[coluna]][2]] #é somente se a coluna que a pessoa escolher for a coluna 0.
 
 
+            self.cubo[0][0][coluna] = coluna3[2]
+            self.cubo[0][1][coluna] = coluna3[1]
+            self.cubo[0][2][coluna] = coluna3[0]
 
-            self.face0[0][coluna] = coluna3[2]
-            self.face0[1][coluna] = coluna3[1]
-            self.face0[2][coluna] = coluna3[0]
-
-            self.face4[0][coluna] = coluna0[2]
-            self.face4[1][coluna] = coluna0[1]
-            self.face4[2][coluna] = coluna0[0]
+            self.cubo[4][tabela_de_tradução[coluna]][0] = coluna0[0]
+            self.cubo[4][tabela_de_tradução[coluna]][1] = coluna0[1]
+            self.cubo[4][tabela_de_tradução[coluna]][2] = coluna0[2]
             
-            self.face2[0][coluna] = coluna1[2]
-            self.face2[1][coluna] = coluna1[1]
-            self.face2[2][coluna] = coluna1[0]
+            self.cubo[2][0][coluna] = coluna1[2]
+            self.cubo[2][1][coluna] = coluna1[1]
+            self.cubo[2][2][coluna] = coluna1[0]
 
-            self.face5[0][coluna] = coluna2[2]
-            self.face5[1][coluna] = coluna2[1]
-            self.face5[2][coluna] = coluna2[0]
+            self.cubo[5][0][tabela_de_tradução[coluna]] = coluna2[0]
+            self.cubo[5][1][tabela_de_tradução[coluna]] = coluna2[1]
+            self.cubo[5][2][tabela_de_tradução[coluna]] = coluna2[2]
 
             if coluna == 0:
-                self.__corrigir_face_adjacente(self.face0)
+                self.__corrigir_face_adjacente(self.cubo[0])
 
             elif coluna == 2:
-                self.__corrigir_face_adjacente(self.face2) #corrigimos rotacionando no sentido anti horário. Neste caso a face está invertida
-                self.__corrigir_face_adjacente(self.face0) #então o movimento no sentido anti horário aqui não resolve, então nós chamamos
-                self.__corrigir_face_adjacente(self.face0) #a função 3 vezes para rotacionarmos no sentido horário
+                self.__corrigir_face_adjacente(self.cubo[2]) #corrigimos rotacionando no sentido horário. Neste caso a face está invertida
+                self.__corrigir_face_adjacente(self.cubo[0]) #então o movimento no sentido anti horário aqui não resolve, então nós chamamos
+                self.__corrigir_face_adjacente(self.cubo[0]) #a função 3 vezes para rotacionarmos no sentido horário
 
         else:
             raise GerarExceção(f"{coluna} não é um número válido para uma coluna. Escolha uma coluna de 0 à 5.")
@@ -201,6 +212,57 @@ class Cubo:
         self.rotacionar_no_sentido_anti_horário_na_vertical(1)
 
 
+    def resolver_o_cubo(self):
+
+        def encontrar_o_centro(cor : str) -> int:
+
+            def verificar_se_a_cor_está_no_centro_da_face(face:list[list[str]]) -> bool:
+
+                if cor == face[1][1]:
+                    return True
+                else:
+                    return False
+            
+            f0 = verificar_se_a_cor_está_no_centro_da_face(self.cubo[0])
+            f1 = verificar_se_a_cor_está_no_centro_da_face(self.cubo[1])
+            f2 = verificar_se_a_cor_está_no_centro_da_face(self.cubo[2])
+            f3 = verificar_se_a_cor_está_no_centro_da_face(self.cubo[3])
+            f4 = verificar_se_a_cor_está_no_centro_da_face(self.cubo[4])
+            f5 = verificar_se_a_cor_está_no_centro_da_face(self.cubo[5])
+
+            if f0:
+                return 0
+            elif f1:
+                return 1
+            elif f2:
+                return 2
+            elif f3:
+                return 3
+            elif f4:
+                return 4
+            elif f5:
+                return 5
+            else:
+                raise GerarExceção("O cubo inserido é inválido")
+            
+        print("==================Passos para resolver o cubo==================")
+        print("1. Deixe a face com o centro amarelo virada para cima e a face com o centro azul virada para você")
+        print("2. A face com o centro azul será a face 0")
+
+
+        amarelo = encontrar_o_centro("amarelo")
+        branco = encontrar_o_centro("branco")
+
+        verde = encontrar_o_centro("verde")
+        azul = encontrar_o_centro("azul")
+
+        amarelo = encontrar_o_centro("vermelho")
+        amarelo = encontrar_o_centro("laranja")
+
+            
+        
+
+
     def definir_cubo(self): #Esta é a função em que nós criamos o nosso cubo
 
         def casos(face):
@@ -232,12 +294,12 @@ class Cubo:
 
 
     def mostrar_cubo(self):
-        print(self.face0)
-        print(self.face1)
-        print(self.face2)
-        print(self.face3)
-        print(self.face4)
-        print(self.face5)
+        print(self.cubo[0])
+        print(self.cubo[1])
+        print(self.cubo[2])
+        print(self.cubo[3])
+        print(self.cubo[4])
+        print(self.cubo[5])
 
 
     def verificar_se_a_face_está_resolvida(self,face):
@@ -262,12 +324,12 @@ class Cubo:
 
         acertos = 0
 
-        acertos = acertos + self.verificar_se_a_face_está_resolvida(self.face0)
-        acertos = acertos + self.verificar_se_a_face_está_resolvida(self.face1)
-        acertos = acertos + self.verificar_se_a_face_está_resolvida(self.face2)
-        acertos = acertos + self.verificar_se_a_face_está_resolvida(self.face3)
-        acertos = acertos + self.verificar_se_a_face_está_resolvida(self.face4)
-        acertos = acertos + self.verificar_se_a_face_está_resolvida(self.face5)
+        acertos = acertos + self.verificar_se_a_face_está_resolvida(self.cubo[0])
+        acertos = acertos + self.verificar_se_a_face_está_resolvida(self.cubo[1])
+        acertos = acertos + self.verificar_se_a_face_está_resolvida(self.cubo[2])
+        acertos = acertos + self.verificar_se_a_face_está_resolvida(self.cubo[3])
+        acertos = acertos + self.verificar_se_a_face_está_resolvida(self.cubo[4])
+        acertos = acertos + self.verificar_se_a_face_está_resolvida(self.cubo[5])
 
         if acertos == 6: #se todas as faces estão corretas
             return True
